1c80ccdcb8f8497cd7a93947ec904ee62e3e5fbd
irosen419 <irosen419@gmail.com>
2020-11-26 20:15:40 -0500

    saves right answer and wrong answer in different Hooks

--- src/Containers/CaptalQuestion.js
+++ src/Containers/CaptalQuestion.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react'
+
 function CapitalQuestion({ countries }) {
+
+    const [answer, setAnswer] = useState(null)
+    const [wrongAnswers, setWrongAnswers] = useState(null)
+
+    useEffect(() => {
+        if (countries) {
+            setAnswer(countries[0])
+            setWrongAnswers(countries.slice(1, 4))
+        }
+    }, [countries])
 
     return (
         <div id="question-card">
             <h1>Capital Question</h1>
-            {countries && countries[0] ? <h2 id="question">{countries[0].capital} is the capital of...</h2> : null}
+            {
+                countries && countries[0] ?
+                    <h2 id="question">{countries[0].capital} is the capital of...</h2>
+                    : null
+            }
+            {wrongAnswers ? console.log(wrongAnswers) : null}
         </div>
     )
 }
